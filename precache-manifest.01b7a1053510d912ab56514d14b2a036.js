fca7e982c203e90fad5e69cc75ecb14c436eb26f
malhotra700 <anantmal007@gmail.com>
2020-08-05 17:54:58 +0530

    Updates

---
+++ precache-manifest.01b7a1053510d912ab56514d14b2a036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9e0016a79617a7e7d62c828bf6859a",
+    "url": "/pics-search/index.html"
+  },
+  {
+    "revision": "532da87602cc9fa30008",
+    "url": "/pics-search/static/css/main.36b554be.chunk.css"
+  },
+  {
+    "revision": "576239bdd47165c3f765",
+    "url": "/pics-search/static/js/2.f88abdb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics-search/static/js/2.f88abdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532da87602cc9fa30008",
+    "url": "/pics-search/static/js/main.64a65145.chunk.js"
+  },
+  {
+    "revision": "315945e7e580eab09141",
+    "url": "/pics-search/static/js/runtime-main.283195d4.js"
+  }
+]);
